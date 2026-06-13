1eb74047910b942e151a51f541c6decdce30e732
Jang Jinuk <wlsdnr123a@gmail.com>
2026-06-13 18:51:26 +0900

    Update site content 2026-06-13 18:51

--- content.js
+++ content.js
@@ -8,28 +8,17 @@ window.LF_CONTENT = {
       "isNew": true
     },
     {
-      "no": "03",
-      "subject": "끊어진 신호는 그대로 보관합니다.",
-      "id": "sysop",
-      "date": "99.12.06"
-    },
-    {
       "no": "02",
-      "subject": "새벽 이후에는 소리가 작게 들립니다.",
+      "subject": "언젠가 돌아옵니다.",
       "id": "node512",
-      "date": "99.12.01"
+      "date": "99.12.01",
+      "isNew": true
     }
   ],
   "diary": [
     {
-      "no": "12",
-      "title": "서랍 안쪽에서 발견한 테이프",
-      "weather": "흐림",
-      "date": "00.01.03"
-    },
-    {
       "no": "11",
-      "title": "저장되지 않은 파일명",
+      "title": "흔적을 찾으러",
       "weather": "눈",
       "date": "99.12.24"
     },
@@ -41,18 +30,6 @@ window.LF_CONTENT = {
     }
   ],
   "music": [
-    {
-      "file": "01_lost.wav",
-      "memo": "아직 열리지 않음",
-      "time": "--:--",
-      "state": "hidden"
-    },
-    {
-      "file": "02_found.wav",
-      "memo": "노이즈 포함",
-      "time": "--:--",
-      "state": "hidden"
-    },
     {
       "file": "readme.txt",
       "memo": "이곳에 트랙 정보를 넣어주세요.",
@@ -66,12 +43,6 @@ window.LF_CONTENT = {
       "filename": "scan_1207.jpg",
       "memo": "색이 많이 바랬음",
       "date": "99.12.07"
-    },
-    {
-      "no": "06",
-      "filename": "room_old.jpg",
-      "memo": "불빛만 남음",
-      "date": "99.11.30"
     }
   ],
   "links": [
